cf0cebafe51f279648595e5f083d34ff4b652485
truggnghia_ <80100031+vtnghia16@users.noreply.github.com>
2023-08-29 23:37:00 +0700

    Combo attack

--- Assets/PlayerPrimaryAttack.cs
+++ Assets/PlayerPrimaryAttack.cs
@@ -1,9 +1,14 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerPrimaryAttack : PlayerState
 {
+    private int comboCounter;
+
+    private float lastTimeAttacked;
+    private float comboWindow = 2;
+
     public PlayerPrimaryAttack(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
     {
     }
@@ -11,11 +16,23 @@ public class PlayerPrimaryAttack : PlayerState
     public override void Enter()
     {
         base.Enter();
+
+        // Set combo tấn công của nhân vật
+        if(comboCounter > 2 || Time.time >= lastTimeAttacked + comboWindow)
+        {
+            comboCounter = 0;
+        }
+
+        player.anim.SetInteger("ComboCounter", comboCounter);
     }
 
     public override void Exit()
     {
         base.Exit();
+
+        comboCounter++;
+        lastTimeAttacked = Time.time;
+
     }
 
     public override void Update()
