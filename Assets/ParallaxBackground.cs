1aaa118a5d17e14429d7dffb70d22ae3ca93d22d
truggnghia_ <80100031+vtnghia16@users.noreply.github.com>
2023-09-05 00:01:06 +0700

    Endless BG

--- Assets/ParallaxBackground.cs
+++ Assets/ParallaxBackground.cs
@@ -1,10 +1,11 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ParallaxBackground : MonoBehaviour
 {
     private GameObject cam;
+    private float length;
 
     [SerializeField] private float parallaxEffect;
 
@@ -13,14 +14,29 @@ public class ParallaxBackground : MonoBehaviour
     void Start()
     {
         cam = GameObject.Find("Main Camera");
+
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
         xPosition = transform.position.x;
     }
 
 
     void Update()
     {
+        float distanceMoved = cam.transform.position.x * (1 - parallaxEffect);
         float distanceToMove = cam.transform.position.x * parallaxEffect;
 
+        // BG dịch chuyển theo nhân vật
         transform.position = new Vector3(xPosition + distanceToMove, transform.position.y);
+
+
+        // BG chạy theo nhân vật
+        if (distanceMoved > xPosition + length)
+        {
+            xPosition = xPosition + length;
+        }
+        else if (distanceMoved < xPosition - length)
+        { 
+            xPosition = xPosition - length;
+        }
     }
 }
