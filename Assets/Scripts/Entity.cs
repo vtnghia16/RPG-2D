e400106000efd1fae91ad9eece25128876e48c2d
truggnghia_ <80100031+vtnghia16@users.noreply.github.com>
2023-09-07 13:25:06 +0700

    On hit impact

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -10,6 +10,10 @@ public class Entity : MonoBehaviour
     public EntityFX fx { get; private set; }
 
     #endregion
+    [Header("Knockback info")]
+    [SerializeField] protected Vector2 knockbackDirection;
+    [SerializeField] protected float knockbackDuration;
+    protected bool isKnocked;
 
     [Header("Collision info")]
     public Transform attackCheck;
@@ -45,15 +49,42 @@ public class Entity : MonoBehaviour
     public virtual void Damage()
     {
         fx.StartCoroutine("FlashFX");
+        StartCoroutine("HitKnockback");
+
         Debug.Log(gameObject.name + "  was damaged!");
     }
 
+    // Hiệu ứng bay lên khi nhân vật bị tấn công
+    protected virtual IEnumerator HitKnockback() 
+    { 
+        isKnocked = true;
+
+        rb.velocity = new Vector2(knockbackDirection.x * -facingDir, knockbackDirection.y);
+
+        yield return new WaitForSeconds(knockbackDuration);
+        isKnocked = false;
+
+    }
+
     #region Velocity
-    public void SetZeroVelocity() => rb.velocity = new Vector2(0, 0);
+    public void SetZeroVelocity()
+    {
+        if(isKnocked)
+        {
+            return;
+        }
+
+        rb.velocity = new Vector2(0, 0);
+    }
 
     // Set tốc độ cho nhân vật
     public void SetVelocity(float _xVelocity, float _yVelocity)
     {
+        if (isKnocked)
+        {
+            return;
+        }
+
         rb.velocity = new Vector2(_xVelocity, _yVelocity);
         FlipController(_xVelocity);
     }
