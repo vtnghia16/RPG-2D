c4427c40472735f739df042252e4f7eaa16c65b9
truggnghia_ <80100031+vtnghia16@users.noreply.github.com>
2023-09-12 00:27:19 +0700

    Clone attack & facing

--- Assets/Clone_Skill_Controller.cs
+++ Assets/Clone_Skill_Controller.cs
@@ -1,17 +1,22 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Clone_Skill_Controller : MonoBehaviour
 {
     private SpriteRenderer sr;
+    private Animator anim;
     [SerializeField] private float colorLoosingSpeed;
 
     private float cloneTimer;
+    [SerializeField] private Transform attackCheck;
+    [SerializeField] private float attackCheckRadius = .8f;
+    private Transform closestEnemy;
 
     private void Awake()
     {
         sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -21,12 +26,74 @@ public class Clone_Skill_Controller : MonoBehaviour
         if (cloneTimer < 0)
         {
             sr.color = new Color(1, 1, 1, sr.color.a - (Time.deltaTime * colorLoosingSpeed));
+
+            if (sr.color.a <= 0)
+            {
+                Destroy(gameObject);
+            }
         }
     }
 
-    public void SetupClone(Transform _newTransform, float _cloneDuration)
+    public void SetupClone(Transform _newTransform, float _cloneDuration, bool _canAttack)
     {
+        if (_canAttack)
+        {
+            anim.SetInteger("AttackNumber", Random.Range(1, 3));
+        }
+
         transform.position = _newTransform.position;
         cloneTimer = _cloneDuration;
+
+
+        FaceClosestTarget();
+    }
+
+    private void AnimationTrigger()
+    {
+        cloneTimer = -.1f;
+    }
+
+    private void AttackTrigger()
+    {
+        // Giới hạn phạm vi tiếp xúc khi tấn công
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(attackCheck.position, attackCheckRadius);
+
+        foreach (var hit in colliders)
+        {
+            if (hit.GetComponent<Enemy>() != null)
+            {
+                hit.GetComponent<Enemy>().Damage();
+            }
+        }
+    }
+
+    // Điều chỉnh hướng quay của nhân vật ảo theo hướng quay của nhân vật thật
+    private void FaceClosestTarget()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 25);
+
+        float closestDistance = Mathf.Infinity;
+
+        foreach (var hit in colliders)
+        {
+            if (hit.GetComponent<Enemy>() != null)
+            {
+                float distanceToEnemy = Vector2.Distance(transform.position, hit.transform.position);
+
+                if (distanceToEnemy < closestDistance)
+                {
+                    closestDistance = distanceToEnemy;
+                    closestEnemy = hit.transform;
+                }
+            }
+        }
+
+        if (closestEnemy != null)
+        {
+            if (transform.position.x > closestEnemy.position.x)
+            {
+                transform.Rotate(0, 180, 0);
+            }
+        }
     }
 }
--- Assets/Scripts/Skills/Clone_Skill.cs
+++ Assets/Scripts/Skills/Clone_Skill.cs
@@ -7,11 +7,13 @@ public class Clone_Skill : Skill
     [Header("Clone info")]
     [SerializeField] private GameObject clonePrefab;
     [SerializeField] private float cloneDuration;
+    [Space]
+    [SerializeField] private bool canAttack;
 
     public void CreateClone(Transform _ClonePosition)
     {
         GameObject newClone = Instantiate(clonePrefab);
 
-        newClone.GetComponent<Clone_Skill_Controller>().SetupClone(_ClonePosition, cloneDuration);
+        newClone.GetComponent<Clone_Skill_Controller>().SetupClone(_ClonePosition, cloneDuration, canAttack);
     }
 }
