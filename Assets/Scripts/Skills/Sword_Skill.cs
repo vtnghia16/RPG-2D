ed79a2638785c28dfd19eb3779b0b94879e9c4a9
truggnghia_ <80100031+vtnghia16@users.noreply.github.com>
2023-09-15 13:26:42 +0700

    Setting up sword aim

--- Assets/Scripts/Skills/Sword_Skill.cs
+++ Assets/Scripts/Skills/Sword_Skill.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,14 +6,91 @@ public class Sword_Skill : Skill
 {
     [Header("Skill info")]
     [SerializeField] private GameObject swordPrefab;
-    [SerializeField] private Vector2 lauchDir;
+    [SerializeField] private Vector2 launchForce;
     [SerializeField] private float swordGravity;
+
+    private Vector2 finalDir;
+
+    // Điều chỉnh hướng của vũ khí
+    [Header("Aim dots")]
+    [SerializeField] private int numberOfDots;
+    [SerializeField] private float spaceBeetwenDots;
+    [SerializeField] private GameObject dotPrefab;
+    [SerializeField] private Transform dotsParent;
+
+    private GameObject[] dots;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        GenerateDots();
+    }
+
+    protected override void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            finalDir = new Vector2(AimDirection().normalized.x * launchForce.x,
+                AimDirection().normalized.y * launchForce.y);
+        }
+
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            for (int i = 0; i < dots.Length; i++)
+            {
+                dots[i].transform.position = DotsPosition(i * spaceBeetwenDots);
+            }
+        }
+    }
 
     public void CreateSword()
     {
         GameObject newSword = Instantiate(swordPrefab, player.transform.position, transform.rotation);
         Sword_Skill_Controller newSwordScript = newSword.GetComponent<Sword_Skill_Controller>();
 
-        newSwordScript.SetupSword(lauchDir, swordGravity);
+        newSwordScript.SetupSword(finalDir, swordGravity);
+
+        DotsActive(false);
+    }
+
+    // Điều chỉnh hướng mục tiêu của vũ khí
+    public Vector2 AimDirection()
+    {
+        Vector2 playerPosition = player.transform.position;
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePosition - playerPosition;
+
+        return direction;
+
+    }
+
+    public void DotsActive(bool _isActive)
+    {
+        for (int i = 0; i < dots.Length; i++)
+        {
+            dots[i].SetActive(_isActive);
+        }
+    }
+
+    // Tạo ra hướng của vũ khí
+    private void GenerateDots()
+    {
+        dots = new GameObject[numberOfDots];
+        for(int i = 0; i < numberOfDots; i++)
+        {
+            dots[i] = Instantiate(dotPrefab, player.transform.position, Quaternion.identity, dotsParent);
+            dots[i].SetActive(false);
+        }
+    }
+
+    // Vị trí của vũ khí
+    private Vector2 DotsPosition(float t)
+    {
+        Vector2 position = (Vector2)player.transform.position + new Vector2(
+            AimDirection().normalized.x * launchForce.x,
+            AimDirection().normalized.y * launchForce.y) * t + .5f * (Physics2D.gravity * swordGravity) * (t * t);
+
+        return position;
     }
 }
