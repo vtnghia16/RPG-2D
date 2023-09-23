78bf3aa7edd8aa7c7a694944766c705d0274ab79
truggnghia_ <80100031+vtnghia16@users.noreply.github.com>
2023-09-23 12:48:24 +0700

    Explosive crystal

--- Assets/Crystal_Skill.cs
+++ Assets/Crystal_Skill.cs
@@ -9,6 +9,14 @@ public class Crystal_Skill : Skill
     [SerializeField] private GameObject crystalPrefab;
     private GameObject currentCrystal;
 
+    [Header("Explosive crystal")]
+    [SerializeField] private bool canExplode;
+
+
+    [Header("Moving crystal")]
+    [SerializeField] private bool canMoveToEnemy;
+    [SerializeField] private float moveSpeed;
+
     public override void UseSkill()
     {
         base.UseSkill();
@@ -18,12 +26,16 @@ public class Crystal_Skill : Skill
             currentCrystal = Instantiate(crystalPrefab, player.transform.position, quaternion.identity);
             Crystal_Skill_Controller currentCrystalScript = currentCrystal.GetComponent<Crystal_Skill_Controller>();
 
-            currentCrystalScript.SetupCrystal(crystalDuration);
+            currentCrystalScript.SetupCrystal(crystalDuration, canExplode, canMoveToEnemy, moveSpeed);
         }
         else
         {
+            Vector2 playerPos = player.transform.position;
+
             player.transform.position = currentCrystal.transform.position;
-            Destroy(currentCrystal);
+
+            currentCrystal.transform.position = playerPos;
+            currentCrystal.GetComponent<Crystal_Skill_Controller>()?.FinishCrystal();
         }
     }
 }
--- Assets/Scripts/Skills/Skill_Controllers/Crystal_Skill_Controller.cs
+++ Assets/Scripts/Skills/Skill_Controllers/Crystal_Skill_Controller.cs
@@ -1,14 +1,27 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Crystal_Skill_Controller : MonoBehaviour
 {
+    private Animator anim => GetComponent<Animator>();
+    private CircleCollider2D cd => GetComponent<CircleCollider2D>();
+
     private float crystalExistTimer;
 
-    public void SetupCrystal(float _crystalDuration)
+    private bool canExplode;
+    private bool canMove;
+    private float moveSpeed;
+
+    private bool canGrow;
+    private float growSpeed = 5;
+
+    public void SetupCrystal(float _crystalDuration, bool _canExplode, bool _canMove, float _moveSpeed)
     {
         crystalExistTimer = _crystalDuration;
+        canExplode = _canExplode;
+        canMove = _canMove;
+        moveSpeed = _moveSpeed;
     }
 
     private void Update()
@@ -16,6 +29,38 @@ public class Crystal_Skill_Controller : MonoBehaviour
         crystalExistTimer -= Time.deltaTime;
 
         if (crystalExistTimer < 0)
+        {
+            FinishCrystal();
+        }
+
+        if(canGrow)
+        {
+            transform.localScale = Vector2.Lerp(transform.localScale, new Vector2(3, 3), growSpeed * Time.deltaTime);
+        }
+    }
+
+    private void AnimationExplodeEvent()
+    {
+        // Giới hạn phạm vi tiếp xúc khi tấn công
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, cd.radius);
+
+        foreach (var hit in colliders)
+        {
+            if (hit.GetComponent<Enemy>() != null)
+            {
+                hit.GetComponent<Enemy>().Damage();
+            }
+        }
+    }
+
+    public void FinishCrystal()
+    {
+        if (canExplode)
+        {
+            canGrow = true;
+            anim.SetTrigger("Explode");
+        }
+        else
         {
             SelfDestroy();
         }
