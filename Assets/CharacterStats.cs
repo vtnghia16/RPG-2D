929d8258897c3ffc7c0fe053b942167befef2736
truggnghia_ <80100031+vtnghia16@users.noreply.github.com>
2023-09-27 22:23:40 +0700

    Elemental ailments

--- Assets/CharacterStats.cs
+++ Assets/CharacterStats.cs
@@ -74,8 +74,44 @@ public class CharacterStats : MonoBehaviour
         totalMagicalDamage = CheckTargetResistance(_targetStats, totalMagicalDamage);
         _targetStats.TakeDamage(totalMagicalDamage);
 
+        if(Mathf.Max(_fireDamage, _iceDamage, _lightingDamage) <= 0)
+        {
+            return;
+        }
+
+        bool canApplyIgnite = _fireDamage > _iceDamage && _fireDamage > _lightingDamage;
+        bool canApplyChill = _iceDamage > _fireDamage && _iceDamage > _lightingDamage;
+        bool canApplyShock = _lightingDamage > _fireDamage && _lightingDamage > _iceDamage;
+
+        while(!canApplyIgnite && !canApplyChill && !canApplyShock)
+        {
+            if(Random.value < .3f && _fireDamage > 0)
+            {
+                canApplyIgnite = true;
+                _targetStats.ApplyAilments(canApplyIgnite, canApplyChill, canApplyShock);
+                Debug.Log("Applied fire");
+                return;
+            }
+
+            if(Random.value < .5f && _iceDamage > 0)
+            {
+                canApplyChill = true;
+                _targetStats.ApplyAilments(canApplyIgnite, canApplyChill, canApplyShock);
+                Debug.Log("Applied ice");
+                return;
+            }
+
+            if(Random.value < .5f && _lightingDamage > 0)
+            {
+                canApplyShock = true;
+                _targetStats.ApplyAilments(canApplyIgnite, canApplyChill, canApplyShock);
+                Debug.Log("Applied lighting");
+                return;
+            }
+        }
 
 
+        _targetStats.ApplyAilments(canApplyIgnite, canApplyChill, canApplyShock);
 
     }
 
