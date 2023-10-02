a2beb303fcc1f0a39f1748859648dc67d2d62a97
truggnghia_ <80100031+vtnghia16@users.noreply.github.com>
2023-10-02 20:41:25 +0700

    Item

---
+++ Assets/ItemObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemObject : MonoBehaviour
+{
+    private SpriteRenderer sr;
+
+    [SerializeField] private ItemData itemData;
+
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+
+        // Hiện vật phẩm
+        sr.sprite = itemData.icon;
+    }
+
+    // Nhặt vật phẩm cho nhân vật
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.GetComponent<Player>() != null)
+        {
+            Debug.Log("Picked up item " + itemData.itemName);
+            Destroy(gameObject);
+        }
+    }
+}
