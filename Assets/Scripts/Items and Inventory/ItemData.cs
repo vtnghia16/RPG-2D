be9b2b96430d96f873ac6c5015b1b94e5d9feaa9
truggnghia_ <80100031+vtnghia16@users.noreply.github.com>
2023-10-04 00:54:38 +0700

    Equipment item

--- Assets/Scripts/Items and Inventory/ItemData.cs
+++ Assets/Scripts/Items and Inventory/ItemData.cs
@@ -1,8 +1,17 @@
-using UnityEngine;
+﻿using UnityEngine;
+
+// Chọn các trang bị vật phẩm
+public enum ItemType
+{
+    Material,
+    Equipment
+}
 
 [CreateAssetMenu(fileName = "New Item Data", menuName = "Data/Item")]
 public class ItemData : ScriptableObject
 {
+    public ItemType itemType;
     public string itemName;
     public Sprite icon;
 }
+
--- Assets/Scripts/UI/UI_HealthBar.cs
+++ Assets/Scripts/UI/UI_HealthBar.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HealthBar_UI : MonoBehaviour
+public class UI_HealthBar : MonoBehaviour
 {
     private Entity entity;
     private CharacterStats myStats;
