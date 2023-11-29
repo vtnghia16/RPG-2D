75a6ab271d9f03b5a2827394423851081636c5dd
truggnghia_ <80100031+vtnghia16@users.noreply.github.com>
2023-11-29 18:04:53 +0700

    Spawn enemy

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject[] enemyPrefabs;
+
+    [Header("Time Intervals")]
+    [SerializeField] private float minSpawnInterval = 10.0f;
+    [SerializeField] private float maxSpawnInterval = 20.0f;
+
+    void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    private IEnumerator SpawnEnemies()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnInterval, maxSpawnInterval));
+
+            // Chọn một prefab ngẫu nhiên từ mảng enemyPrefabs
+            GameObject randomPrefab = enemyPrefabs[Random.Range(0, enemyPrefabs.Length)];
+
+            // Kiểm tra xem prefab đã bị hủy chưa
+            if (randomPrefab != null)
+            {
+                // Tính toán vị trí spawn dựa trên vị trí của người chơi và offset
+                Vector3 spawnPosition = new Vector3(player.transform.position.x + 5 + Random.Range(-10, 20) , 8, 0);
+
+                // Spawn enemy sử dụng prefab được chọn và vị trí ngẫu nhiên
+                GameObject newEnemy = Instantiate(randomPrefab, spawnPosition, Quaternion.identity);
+            }
+            else
+            {
+                Destroy(randomPrefab);
+                Debug.LogError("Prefab is null!");
+            }
+        }
+    }
+}
+
--- Assets/Scripts/Stats/EnemyStats.cs
+++ Assets/Scripts/Stats/EnemyStats.cs
@@ -8,39 +8,16 @@ public class EnemyStats : CharacterStats
     private ItemDrop myDropSystem; // Hệ thống rơi của vật phẩm
     public Stat soulsDropAmount;
 
-    [Header("Level details")]
-    [SerializeField] private int level = 1;
-
-    [Range(0f, 1f)]
-    [SerializeField] private float percantageModifier = .4f;
 
     protected override void Start()
     {
         soulsDropAmount.SetDefaultValue(1);
-        // ApplyLevelModifiers();
+        //ApplyLevelModifiers();
 
         base.Start();
 
         enemy = GetComponent<Enemy>();
         myDropSystem = GetComponent<ItemDrop>();
-    }
-
-    // Chỉnh sửa Level của quái vật
-    private void ApplyLevelModifiers()
-    {
-        Modify(damage);
-        Modify(maxHealth);
-        Modify(armor);
-    }
-
-    private void Modify(Stat _stat)
-    {
-        for (int i = 1; i < level; i++)
-        {
-            float modifier = _stat.GetValue() * percantageModifier;
-
-            _stat.AddModifier(Mathf.RoundToInt(modifier));
-        }
     }
 
     public override void TakeDamage(int _damage)
