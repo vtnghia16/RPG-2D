1f9c0a6a25948efe5e477f70ca9b17ef3241fcb5
truggnghia_ <80100031+vtnghia16@users.noreply.github.com>
2023-11-29 14:50:03 +0700

    fix

--- Assets/Scripts/Items and Inventory/ItemData_Equipment.cs
+++ Assets/Scripts/Items and Inventory/ItemData_Equipment.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -21,27 +21,10 @@ public class ItemData_Equipment : ItemData
     public ItemEffect[] itemEffects;
 
 
-    //[Header("Major stats")]
-    //public int strength;
-    //public int agility;
-    //public int intelligence;
-    //public int vitality;
-
-    [Header("Offensive stats")]
+    [Header("Stats")]
     public int damage;
-    //public int critChance;
-    //public int critPower;
-
-    [Header("Defensive stats")]
     public int health;
     public int armor;
-    //public int evasion;
-    //public int magicResistance;
-
-    //[Header("Magic stats")]
-    //public int fireDamage;
-    //public int iceDamage;
-    //public int lightingDamage;
 
 
     [Header("Craft requirements")]
@@ -62,49 +45,20 @@ public class ItemData_Equipment : ItemData
     {
         PlayerStats playerStats = PlayerManager.instance.player.GetComponent<PlayerStats>();
 
-        //playerStats.strength.AddModifier(strength);
-        //playerStats.agility.AddModifier(agility);
-        //playerStats.intelligence.AddModifier(intelligence);
-        //playerStats.vitality.AddModifier(vitality);
-
         playerStats.damage.AddModifier(damage);
-        //playerStats.critChance.AddModifier(critChance);
-        //playerStats.critPower.AddModifier(critPower);
-
         playerStats.maxHealth.AddModifier(health);
         playerStats.armor.AddModifier(armor);
-        //playerStats.evasion.AddModifier(evasion);
-        //playerStats.magicResistance.AddModifier(magicResistance);
 
-        //playerStats.fireDamage.AddModifier(fireDamage);
-        //playerStats.iceDamage.AddModifier(iceDamage);
-        //playerStats.lightingDamage.AddModifier(lightingDamage);
     }
 
     public void RemoveModifiers() 
     {
         PlayerStats playerStats = PlayerManager.instance.player.GetComponent<PlayerStats>();
 
-        //playerStats.strength.RemoveModifier(strength);
-        //playerStats.agility.RemoveModifier(agility);
-        //playerStats.intelligence.RemoveModifier(intelligence);
-        //playerStats.vitality.RemoveModifier(vitality);
-
-
         playerStats.damage.RemoveModifier(damage);
-        //playerStats.critChance.RemoveModifier(critChance);
-        //playerStats.critPower.RemoveModifier(critPower);
-        
-
         playerStats.maxHealth.RemoveModifier(health);
         playerStats.armor.RemoveModifier(armor);
-        //playerStats.evasion.RemoveModifier(evasion);
-        //playerStats.magicResistance.RemoveModifier(magicResistance);
 
-
-        //playerStats.fireDamage.RemoveModifier(fireDamage);
-        //playerStats.iceDamage.RemoveModifier(iceDamage);
-        //playerStats.lightingDamage.RemoveModifier(lightingDamage);
     }
 
     public override string GetDescription()
@@ -112,37 +66,22 @@ public class ItemData_Equipment : ItemData
         sb.Length = 0;
         descriptionLength = 0;
 
-        //AddItemDescription(strength, "Strength");
-        //AddItemDescription(agility, "Agility");
-        //AddItemDescription(intelligence, "Intelligence");
-        //AddItemDescription(vitality, "Vitality");
 
         AddItemDescription(damage, "Damage");
-        //AddItemDescription(critChance, "Crit.Chance");
-        //AddItemDescription(critPower, "Crit.Power");
-
         AddItemDescription(health, "Health");
-        //AddItemDescription(evasion, "Evasion");
         AddItemDescription(armor, "Armor");
-        //AddItemDescription(magicResistance, "Magic Resist.");
-
-        //AddItemDescription(fireDamage, "Fire damage");
-        //AddItemDescription(iceDamage, "Ice damage");
-        //AddItemDescription(lightingDamage, "Lighting dmg. ");
-
-
-
 
 
         for (int i = 0; i < itemEffects.Length; i++)
         {
-            if (itemEffects[i].effectDescription.Length > 0)
+            if (itemEffects[i] != null && !string.IsNullOrEmpty(itemEffects[i].effectDescription))
             {
                 sb.AppendLine();
-                sb.AppendLine("Unique: " + itemEffects[i].effectDescription);
+                sb.AppendLine("Mô tả: " + itemEffects[i].effectDescription);
                 descriptionLength++;
             }
         }
+
 
 
         if (descriptionLength < 5)
@@ -154,8 +93,6 @@ public class ItemData_Equipment : ItemData
             }
         }
 
-
-        
         return sb.ToString();
     }
 
